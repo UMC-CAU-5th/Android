985fbcf646b6d658671b994882c97cac8a15b50b
riadan710 <riadan710@gmail.com>
2023-10-01 19:20:58 +0900

    FEAT : Week2 실습 완료

--- Marco/Flo_clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
+++ Marco/Flo_clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.flo_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.flo_clone.databinding.ActivityMainBinding
 
@@ -13,7 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+
+        binding.mainPlayerCl.setOnClickListener {
+            //startActivity(Intent(this, SongActivity::class.java))
+            val intent = Intent(this,SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer", song.singer)
+            startActivity(intent)
+        }
         initBottomNavigation()
+
+        Log.d("Song", song.title + song.singer)
     }
 
     private fun initBottomNavigation(){
