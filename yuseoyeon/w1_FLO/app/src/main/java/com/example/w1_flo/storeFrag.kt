50982c2248cf1302f44d016ce0db5f8238f305bf
yuseoyeon <yusy0425@naver.com>
2023-10-08 19:54:33 +0900

    week3 미션

--- yuseoyeon/w1_FLO/app/src/main/java/com/example/w1_flo/storeFrag.kt
+++ yuseoyeon/w1_FLO/app/src/main/java/com/example/w1_flo/storeFrag.kt
@@ -5,55 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.w1_flo.databinding.FragmentStoreBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [storeFrag.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class storeFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var information= arrayListOf("저장한 곡","음악파일")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding:FragmentStoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false)
+        binding=FragmentStoreBinding.inflate(layoutInflater,container,false)
+
+        val storeAdapter=StoreVPAdapter(this)
+        binding.storeVp.adapter=storeAdapter
+
+        TabLayoutMediator(binding.storeTb,binding.storeVp){
+            tab, position->
+            tab.text=information[position]
+        }.attach()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment storeFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            storeFrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
